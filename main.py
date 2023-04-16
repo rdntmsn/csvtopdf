db8b99230a0d89b87e0449659f1847885d6f00b0
rdntmsn <rdntmsn@gmail.com>
2023-04-16 11:52:50 +0100

    added note pages for each topic

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ pdf = FPDF(orientation='P', unit='mm', format='A4', )
 
 for index, row in df.iterrows():
     topics = row['Topic']
-    # num_of_pages = row['Pages']
+    num_of_pages = row['Pages']
 
     pdf.add_page()
     pdf.set_font(family='Helvetica', style='B', size=16)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=16, txt=topics, align='L', ln=1)
     pdf.line(x1=10, y1=23, x2=200, y2=23)
-    # pdf.cell(w=0, h=12, txt='Lists', align='L', ln=1)
+    for page in range(num_of_pages):
+        pdf.add_page()
+        pdf.set_font(family='Helvetica', style='B', size=12)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=f"Notes for {topics}", align='L', ln=1)
+        pdf.line(x1=10, y1=23, x2=200, y2=23)
+
 pdf.output("python_notes.pdf")
